2e6d92785697dbb1b8887c6fc55faf9ce0364655
meronmks <meronmks.8914@gmail.com>
2017-10-14 16:01:09 +0900

    Fix Encoding

--- 003.py
+++ 003.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # 円周率
-
 if __name__ == "__main__":
     str1 = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
     str1 = str1.replace(",", "")
